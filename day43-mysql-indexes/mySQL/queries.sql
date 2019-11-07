d758cb0c9e679fc061eef16abb786ef9067b073a
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-07 13:19:14 +0100

    day43

---
+++ day43-mysql-indexes/mySQL/queries.sql
@@ -0,0 +1,32 @@
+EXPLAIN
+SELECT *
+FROM `paragraphs`
+WHERE `id` = 1337;
+
+EXPLAIN
+SELECT *
+FROM `words`
+WHERE `word` = 'kissed';
+
+INSERT INTO `words`
+(`word`, `count`)
+VALUES
+(`february`, 10);
+
+EXPLAIN
+SELECT *
+FROM `paragraphs`
+WHERE `text` LIKE '%Edmond Dantès%';
+-- 41 results
+
+SELECT *,
+    MATCH (`text`) AGAINST ('Edmond Dantès')
+FROM `paragraphs`
+WHERE MATCH (`text`) AGAINST ('Edmond Dantès');
+-- 788 results
+
+SELECT *,
+    MATCH (`text`) AGAINST ('Edmond Dantès')
+FROM `paragraphs`
+WHERE MATCH (`text`) AGAINST ('"Edmond Dantès"');
+-- 41 results, where are searching exactly for "Edmond Dantès"
