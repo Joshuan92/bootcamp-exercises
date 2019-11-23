0d1b35c8a56ade62e22f06081a95b4f6486bc7df
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-23 16:11:50 +0100

    updated gitignore

---
+++ day53-laravel/news-app/storage/framework/views/87cbbef010fd7a676eacdf11e17ef641a8fbf76e.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+    <link rel="stylesheet" type="text/css" href="./../css/app.css" />
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+
+    <title>News</title>
+</head>
+<body>
+
+    <div id="app"></div>
+  
+
+    
+    <script src="<?php echo e(mix('js/app.js')); ?>"></script>
+    
+  
+</body>
+</html>
+
+<?php /**PATH C:\webDevelopment\Bootcamp\exercises\day53-laravel\news-app\resources\views/layout.blade.php ENDPATH**/ ?>
