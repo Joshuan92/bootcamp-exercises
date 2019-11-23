44bb991872e43c4314bc61b3eef7e831b8434fb8
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-23 14:34:42 +0100

    scraping of Sportisimo

---
+++ day53-laravel/news-app/resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+    <link rel="stylesheet" type="text/css" href="./../css/app.css" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <title>News</title>
+</head>
+<body>
+
+    <div id="app"></div>
+  
+
+    
+    <script src="{{ mix('js/app.js') }}"></script>
+    
+  
+</body>
+</html>
+
